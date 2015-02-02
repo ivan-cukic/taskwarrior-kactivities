a243a215e41d7f0a3f917229e3ee7188484ed044
Ivan Čukić <ivan.cukic@kde.org>
2015-02-02 21:25:39 +0100

    Fixed the problem with the project name on task modification commands

--- task.py
+++ task.py
@@ -33,6 +33,7 @@ def is_project_tag(tag):
     return tag.startswith("project:")
 
 # By default, using the current activity name as the project
+project_tag_present = False
 project_name = "project:" + current_activity_slug()
 
 other_tags, project_tag = partition(is_project_tag, sys.argv[1:])
@@ -42,6 +43,7 @@ other_tags, project_tag = partition(is_project_tag, sys.argv[1:])
 # underscores, but task warrior sometimes thinks we want to
 # subtract strings...
 for arg in project_tag:
+    project_tag_present = True
     project_name = arg.replace("project:",
                                "project:" + current_activity_slug() + ":")
 
@@ -49,17 +51,39 @@ for arg in project_tag:
 command = [taskwarrior_bin]
 
 # If the first argument is 'all', then do not add the activity tag
-other_tags = list(other_tags)
+other_tags   = list(other_tags)
+all_mode     = other_tags and other_tags[0] == "all"
+task_id_mode = other_tags and other_tags[0].isdigit()
 
-if other_tags and other_tags[0] == "all":
-    other_tags = other_tags[1:]
+if all_mode:
+    # If the first argument is a number, it is (probably)
+    # the id of a task, no need to do anything about the
+    # project name. The task id identifies a task regardless
+    # of the current activity.
+    # We only need to act if one of the arguments was the
+    # project
+
+    # Removing the first argument ('all') if exists
+    if other_tags:
+        other_tags = other_tags[1:]
+
+elif task_id_mode:
+    # If the first argument is a number, it is (probably) the id
+    # of a task, no need to do anything about the project name
+    # unless the user has specified a project. In that case, we
+    # still want to tag it with the activity name
+    if project_tag_present:
+        other_tags = other_tags + [project_name]
+
 else:
+    # Otherwise add the project tag to the start
     command = command + [project_name]
 
 # Arguments to pass apart from the project tag
 args = command + other_tags
 
 # Running the task warrior
+# print(args)
 call(args)
 
 
